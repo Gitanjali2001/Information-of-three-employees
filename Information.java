f47612a52090c2846cedd6d31c0cf9b7f20ecc1f
Gitanjali2001 <69495628+Gitanjali2001@users.noreply.github.com>
2021-09-30 01:04:10 +0530

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,20 @@
+class information
+
+{
+
+public static void main (String args[])
+
+{
+
+System.out.println("Name Year of joining Address");
+
+System.out.println("Robert 1994 64C- WallsStreat");
+
+System.out.println("Sam 2000 68D- WallsStreat");
+
+System.out.println("John 1999 26B- WallsStreat");
+
+}
+
+}
+
